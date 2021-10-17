c530dc30beba0d3831ec8e4b6b8df6a0f5849884
maxcutlyp <max@maxcutlyp.com>
2021-10-17 14:20:27 +0800

    general code cleanup, added rudimentary requirements.txt, bot now plays in vc

--- youtubebot.py
+++ youtubebot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.9
+
 import discord
 from discord.ext import commands
 import yt_dlp
@@ -6,6 +8,7 @@ import asyncio
 import threading
 
 bot = commands.Bot(command_prefix='.')
+queues = {} # {server_id: [vid_id, ...]}
 
 def main():
     with open('./token.txt') as t:
@@ -13,16 +16,21 @@ def main():
         token = lines[0][:-1]
     bot.run(token)
 
-@bot.command(name='play')
+@bot.command(name='play', aliases=['p'])
 async def play(ctx, *args):
     query = ' '.join(args)
 
     # this is how it's determined if the url is valid (i.e. whether to search or not) under the hood of yt-dlp
     will_need_search = not urllib.parse.urlparse(query).scheme
 
+    vc = ctx.author.voice 
+    if vc is None:
+        await ctx.send('you have to be in a vc to play something')
+        return
+
     # source address as 0.0.0.0 to force ipv4 because ipv6 breaks it for some reason
     # this is equivalent to --force-ipv4 (line 312 of https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/options.py)
-    await ctx.send(f'Looking for `{query}`...')
+    await ctx.send(f'looking for `{query}`...')
     with yt_dlp.YoutubeDL({'format': 'worstaudio',
                            'source_address': '0.0.0.0',
                            'default_search': 'ytsearch',
@@ -32,18 +40,15 @@ async def play(ctx, *args):
                            # 'progress_hooks': [lambda info, ctx=ctx: video_progress_hook(ctx, info)],
                            # 'match_filter': lambda info, incomplete, will_need_search=will_need_search, ctx=ctx: start_hook(ctx, info, incomplete, will_need_search),
                            'paths': {'home': './dl/'}}) as ydl:
-        # todo: delete all this shit and just extract data if required for title (!will_need_search) and then do the download synchronously
         info = ydl.extract_info(query, download=False)
         if 'entries' in info:
             info = info['entries'][0]
-        if will_need_search:
-            # send link as input was a search
-            message = f'Downloading https://youtu.be/{info["id"]}'
-        else:
-            # send title as link has already been sent, sending again would clutter chat with previews
-            message = f'Downloading `{info["title"]}`'
-        await ctx.send(message)
+        # send link if it was a search, otherwise send title as sending link again would clutter chat with previews
+        await ctx.send('downloading ' + (f'https://youtu.be/{info["id"]}' if will_need_search else f'`{info["title"]}`'))
         ydl.download([query])
+        connection = await vc.channel.connect()
+        connection.play(discord.FFmpegOpusAudio(f'./dl/{info["id"]}.{info["ext"]}'))
+
 
 #def ydl_download_in_new_thread(ydl, query):
 #    def run_in_asyncio_loop(ydl, query):
